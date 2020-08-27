c0c8f4655066c7daf7655c9292f13f03f8cda839
Vasil <vasil.silianov@gmail.com>
2020-08-27 12:08:32 +0300

    Implementation of conversuonRestController working!

---
+++ src/main/java/com/example/demo/controllers/rest/ConversionRestController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controllers.rest;
+
+import com.example.demo.models.Transaction;
+import com.example.demo.services.contracts.ConversionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/conversion")
+public class ConversionRestController {
+    private ConversionService conversionService;
+
+    @Autowired
+    public ConversionRestController(ConversionService conversionService) {
+        this.conversionService = conversionService;
+    }
+
+
+    @PostMapping("/{sourceAmount}/{sourceCurrency}/{targetCurrency}")
+    public String currencyConversion(@PathVariable double sourceAmount, @PathVariable String sourceCurrency, @PathVariable String targetCurrency) {
+        Transaction transaction = conversionService.currencyConversion(sourceAmount, sourceCurrency, targetCurrency);
+        return String.format("Transaction ID: %d \nTarget currency amount: %.5f", transaction.getTransactionId(), transaction.getAmount());
+    }
+
+}
+
