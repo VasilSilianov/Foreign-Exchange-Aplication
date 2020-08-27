1ef7c9967bc0a3fc44680892eed9f46f1629de6d
Vasil <vasil.silianov@gmail.com>
2020-08-27 22:09:57 +0300

    implementation of method getAllByDate

--- src/main/java/com/example/demo/controllers/rest/ConversionRestController.java
+++ src/main/java/com/example/demo/controllers/rest/ConversionRestController.java
@@ -3,10 +3,9 @@ package com.example.demo.controllers.rest;
 import com.example.demo.models.Transaction;
 import com.example.demo.services.contracts.ConversionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/conversion")
@@ -18,6 +17,19 @@ public class ConversionRestController {
         this.conversionService = conversionService;
     }
 
+
+    @GetMapping("/transaction/{id}")
+    public String getTransactionById(@PathVariable int id){
+        Transaction transaction= conversionService.getById(id);
+        return  String.format("Transaction ID: %d\n" +
+                              "Amount: %.5f\n" +
+                              "Date: %s",transaction.getTransactionId(),transaction.getAmount(),transaction.getDate()) ;
+    }
+
+    @GetMapping("transactions/{date}")
+    public List<Transaction> getAllTransactionsAtSpecificDate(@PathVariable String date){
+       return  conversionService.getByDate(date);//List<Transaction> transactions =
+    }
 
     @PostMapping("/{sourceAmount}/{sourceCurrency}/{targetCurrency}")//todo DTO za da gi napavam kato Json
     public String currencyConversion(@PathVariable double sourceAmount, @PathVariable String sourceCurrency, @PathVariable String targetCurrency) {
