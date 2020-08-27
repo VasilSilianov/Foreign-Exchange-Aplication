21284cf9f7934277dbafa538cab521b7bde63799
Vasil <vasil.silianov@gmail.com>
2020-08-27 21:27:04 +0300

    implementation of getByTransactionId and getByDate

--- src/main/java/com/example/demo/repositiries/TransactionRepository.java
+++ src/main/java/com/example/demo/repositiries/TransactionRepository.java
@@ -4,6 +4,11 @@ import com.example.demo.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction,Integer> {
+
+    Transaction getByTransactionId(int transactionId);
+    List<Transaction> getByDate(String date);
 }
